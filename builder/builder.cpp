11d1955c1ec5bbe35e4e34bc175fe1c4f7aa8d72
Kotherine <astapchukep@student.bmstu.ru>
2022-05-29 12:07:09 +0300

    final version

---
+++ builder/builder.cpp
@@ -0,0 +1,7 @@
+// Copyright 2021 Your Name <your_email>
+
+#include "my_cmake.hpp"
+
+int main(int argc, char* argv[]) {
+  start_cmake(argc, argv);
+}
---
+++ include/my_cmake.hpp
@@ -0,0 +1,30 @@
+// Copyright 2021 Your Name <your_email>
+
+#ifndef INCLUDE_MY_CMAKE_HPP_
+#define INCLUDE_MY_CMAKE_HPP_
+
+#include <async++.h>
+#include <boost/process.hpp>
+#include <boost/process/extend.hpp>
+#include <boost/program_options.hpp>
+#include <vector>
+#include <iostream>
+#include <signal.h>
+#include <string>
+#include <thread>
+#include <chrono>
+
+namespace processes = boost::process;
+namespace po = boost::program_options;
+
+void start_cmake(int argc, char* argv[]);
+
+void create_child(const std::string& command, const time_t& period);
+
+void create_child(const std::string& command, const time_t& period, int& res);
+
+void check_time(processes::child& process, const time_t& period);
+
+time_t time_now();
+
+#endif // INCLUDE_MY_CMAKE_HPP_
